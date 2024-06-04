ee3ab862e53c51440dca575557c5f229cc6412ad
Ilya Burinsky <burinskiy992@gmail.com>
2024-06-04 21:30:14 +0300

    wip: SubscriptionHandler.Subscribe

---
+++ internal/handlers/subscription.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/ilya-burinskiy/birthday-notify/internal/middlewares"
+	"github.com/ilya-burinskiy/birthday-notify/internal/models"
+	"go.uber.org/zap"
+)
+
+type SubscribeService interface {
+	Subscribe(ctx context.Context, subscribedUserID, subscribingUserID int) (models.Subscription, error)
+}
+
+type SubscriptionHandler struct {
+	logger *zap.Logger
+}
+
+func NewSubscriptionHandler(logger *zap.Logger) SubscriptionHandler {
+	return SubscriptionHandler{
+		logger: logger,
+	}
+}
+
+func (h SubscriptionHandler) Subscribe(subscribeSrv SubscribeService) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		subscribingUserID, _ := middlewares.UserIDFromContext(r.Context())
+		subscribedUserID, err := strconv.Atoi(chi.URLParam(r, "id"))
+		if err != nil {
+			h.logger.Info("invalid user id", zap.Error(err))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		_, err = subscribeSrv.Subscribe(context.Background(), subscribedUserID, subscribingUserID)
+		// TODO: respond with proper http status
+		if err != nil {
+			h.logger.Info("failed to subscribe user", zap.Error(err))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
