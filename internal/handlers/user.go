2a55a22b788638a72feadd924fe9cb7bef38a922
Ilya Burinsky <burinskiy992@gmail.com>
2024-06-04 19:34:07 +0300

    UserHandler.Authenticate

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -16,6 +16,10 @@ type RegisterService interface {
 	Register(ctx context.Context, email, password string, birthDate time.Time) (string, error)
 }
 
+type AuthenticateService interface {
+	Authenticate(ctx context.Context, email, password string) (string, error)
+}
+
 type UserHandler struct {
 	logger *zap.Logger
 }
@@ -65,6 +69,39 @@ func (h UserHandler) Register(regSrv RegisterService) func(http.ResponseWriter, 
 			}
 			return
 		}
+		auth.SetJWTCookie(w, jwtStr)
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func (h UserHandler) Authenticate(authService AuthenticateService) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		type payload struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+		var requestBody payload
+		decoder := json.NewDecoder(r.Body)
+		encoder := json.NewEncoder(w)
+		err := decoder.Decode(&requestBody)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			if err := encoder.Encode("invalid request body"); err != nil {
+				h.logger.Info("failed to encode response", zap.Error(err))
+			}
+			return
+		}
+		jwtStr, err := authService.Authenticate(r.Context(), requestBody.Email, requestBody.Password)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			if err := encoder.Encode(err.Error()); err != nil {
+				h.logger.Info("failed to encode response", zap.Error(err))
+			}
+			return
+		}
+
 		auth.SetJWTCookie(w, jwtStr)
 		w.WriteHeader(http.StatusOK)
 	}
