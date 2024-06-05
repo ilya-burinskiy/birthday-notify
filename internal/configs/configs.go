9c9708efa38c3b0f47df58c17641a658c1f03456
Ilya Burinsky <burinskiy992@gmail.com>
2024-06-05 14:27:03 +0300

    added smtp configs

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -11,6 +11,12 @@ const SecretKey = "secret"
 type Config struct {
 	RunAddr string
 	DSN     string
+
+	SMTPAuthIdentity string
+	SMTPAuthUsername string
+	SMTPAuthPassword string
+	SMTPHost         string
+	SMTPPort         string
 }
 
 func Parse() Config {
@@ -24,6 +30,22 @@ func Parse() Config {
 	}
 	if envDSN := os.Getenv("DATABASE_URI"); envDSN != "" {
 		config.DSN = envDSN
+	}
+
+	if envSMTPAuthIdentity := os.Getenv("SMTP_AUTH_IDENTITY"); envSMTPAuthIdentity != "" {
+		config.SMTPAuthIdentity = envSMTPAuthIdentity
+	}
+	if envSMTPAuthUsername := os.Getenv("SMTP_AUTH_USERNAME"); envSMTPAuthUsername != "" {
+		config.SMTPAuthUsername = envSMTPAuthUsername
+	}
+	if envSMTPAuthPassword := os.Getenv("SMTP_AUTH_PASSWORD"); envSMTPAuthPassword != "" {
+		config.SMTPAuthPassword = envSMTPAuthPassword
+	}
+	if envSMTPHost := os.Getenv("SMTP_HOST"); envSMTPHost != "" {
+		config.SMTPHost = envSMTPHost
+	}
+	if envSMTPPort := os.Getenv("SMTP_PORT"); envSMTPPort != "" {
+		config.SMTPPort = envSMTPPort
 	}
 
 	return config
