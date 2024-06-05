8d7205d9600bfa019f3461396989853538ce36a2
Ilya Burinsky <burinskiy992@gmail.com>
2024-06-05 20:04:28 +0300

    added json tags to notify setting model

--- internal/models/notifysetting.go
+++ internal/models/notifysetting.go
@@ -1,7 +1,7 @@
 package models
 
 type NotifySetting struct {
-	ID               int
-	UserID           int
-	DaysBeforeNotify int
+	ID               int `json:"id"`
+	UserID           int `json:"user_id"`
+	DaysBeforeNotify int `json:"days_before_notify"`
 }
